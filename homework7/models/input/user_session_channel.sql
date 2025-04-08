62b56054408e19488092716b4d5aa8013f6db9f7
Chaitanya <CHAITANYAMSC13@GMAIL.COM>
2025-04-07 21:48:58 -0700

    Update user_session_channel.sql

--- homework7/models/input/user_session_channel.sql
+++ homework7/models/input/user_session_channel.sql
@@ -1,9 +1,13 @@
-SELECT
-    userId,
-    sessionId,
-    channel
-FROM {{ source('raw', 'user_session_channel') }}
-WHERE sessionID IS NOT NULL
+WITH src_user_session_channel AS (
+    SELECT
+        userId,
+        sessionId,
+        channel
+    FROM {{ source('raw', 'user_session_channel') }}
+    WHERE sessionID IS NOT NULL
+)
+
+SELECT * FROM src_user_session_channel
 
 
 
